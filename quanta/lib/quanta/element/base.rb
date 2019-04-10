a7ab6f8e28d3e600c689431346271b846d971749
Abhishek Sarkar <abhisheksarka@gmail.com>
2019-04-10 20:05:55 +0530

    changes

--- quanta/lib/quanta/element/base.rb
+++ quanta/lib/quanta/element/base.rb
@@ -1,6 +1,8 @@
 module Quanta
   module Element
     class Base
+      SIGNATURE_ATTRS = %i[id class type].freeze
+
       attr_accessor :el
 
       def initialize(el)
@@ -9,6 +11,20 @@ module Quanta
 
       def broadcast_change
         el.fire_event('change')
+      end
+
+      def attrs
+        el.attributes
+      end
+
+      def signature_attrs
+        attrs.with_indifferent_access.slice(*SIGNATURE_ATTRS)
+      end
+
+      def signature
+        {
+          el.tag_name.to_sym => signature_attrs
+        }
       end
     end
   end
--- quanta/lib/quanta/element/check_box.rb
+++ quanta/lib/quanta/element/check_box.rb
@@ -2,7 +2,8 @@ module Quanta
   module Element
     class CheckBox < Base
       def set(value)
-
+        value ? el.set : el.clear
+        broadcast_change
       end
     end
   end
