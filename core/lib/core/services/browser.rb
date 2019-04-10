79cc034bae9d154f77fb740b3dd3b66fd0c0f5b9
Abhishek Sarkar <abhisheksarka@gmail.com>
2019-04-10 22:20:12 +0530

    removed pry

--- core/lib/core/services/browser.rb
+++ core/lib/core/services/browser.rb
@@ -21,7 +21,7 @@ module Core
 
           flow.steps.each do |step|
             next unless step.execute?(binding)
-            binding.pry if step.name == 'Property Type'
+            
             send(step.step_type, step.interpolated_config(binding))
           end
         end
