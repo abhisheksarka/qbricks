0ee5006405df876df5fec955a463965dab166427
Abhishek Sarkar <abhisheksarka@gmail.com>
2019-04-16 22:06:50 +0530

    proper ordering

--- core/lib/core/models/flow.rb
+++ core/lib/core/models/flow.rb
@@ -1,5 +1,5 @@
 class Flow < ApplicationRecord
   include Conditionable
   belongs_to :site
-  has_and_belongs_to_many :steps
+  has_and_belongs_to_many :steps, -> { order('flows_steps.serial asc') }
 end
