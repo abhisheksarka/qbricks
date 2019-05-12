5c26329d920d1d55630272674b4d09638f421e62
Abhishek Sarkar <abhisheksarka@gmail.com>
2019-05-12 23:03:20 +0530

    trying to fix buggy uploads

--- quanta/lib/quanta/support/file.rb
+++ quanta/lib/quanta/support/file.rb
@@ -10,9 +10,9 @@ module Quanta
     module File
       class << self
         def remote_to_local(url)
-          tmp = open(url)
+          io = open(url)
           file = Tempfile.new([Url.file_name(url), ".#{Url.file_extension(url)}"])
-          file.write(tmp.read)
+          FileUtils.mv io.path, file.path
           file
         end
       end
